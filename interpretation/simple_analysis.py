cbc4fe7e58183b646dec619a97bd4c952cf63457
Bundaberg-Joey <44094475+Bundaberg-Joey@users.noreply.github.com>
2018-12-13 22:48:32 +0000

    updated simple_analysis.py
    Updated comments

--- interpretation/simple_analysis.py
+++ interpretation/simple_analysis.py
@@ -9,7 +9,7 @@ def ir_database(path_to_file='ir_frequencies.txt'):
 
 
 ########################################################################################################################
-# Function : takes user of frequencies and returns a list when user specifies is complete
+# Function : takes user frequencies and returns them in a list when user specifies is complete
 
 
 def user_frequencies():
@@ -25,7 +25,7 @@ def user_frequencies():
 
 
 ########################################################################################################################
-# Function : Takes user input as input then parses loaded file for relevant rows, will then return relevant rows
+# Function : Takes user input as input then parses loaded IR database for relevant rows, will then return relevant rows
 
 
 def ir_database_parser(user_input_list, ir_data):
@@ -39,18 +39,19 @@ def ir_database_parser(user_input_list, ir_data):
 
 
 ########################################################################################################################
+# Function : Takes nested list as input and writes the result to a txt file with name specified by the user
 
 
 def results_writer(filtered_ir):
     while True:
-        user_file_name = input('Please enter valid file name ')
+        user_file_name = input('Please enter a file name to save results to: ')
         if user_file_name.isalpha():
             print('Filename saved to :  '+user_file_name+'.txt')
             break
         else:
-            print('Please enter valid filename')
+            print('Please enter valid filename: ')
 
-    with open(user_file_name+'.txt', 'w') as f:
+    with open(user_file_name+'.txt', 'w') as f:  # writes nested array as a pipe delimited file
         for line in filtered_ir:
             for entry in line:
                 f.write(str(entry)+'|')
@@ -63,7 +64,7 @@ def results_writer(filtered_ir):
 
 def main():
     returned_analysis = ir_database_parser(user_frequencies(), ir_database())  # create filtered list of wavenumbers
-    results_writer(returned_analysis)
+    results_writer(returned_analysis)  # write the filtered list of wavenumbers to a file
 
 
 ########################################################################################################################
