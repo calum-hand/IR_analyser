a938adbe9a1c7ec197714a403b441b50aaf2054d
BundabergJoey <calumhand94@hotmail.com>
2019-07-14 23:49:55 +0100

    file now accepts command line as input

--- interpretation/analyser.py
+++ interpretation/analyser.py
@@ -1,21 +1,16 @@
 import pandas as pd
+import click
 
 ########################################################################################################################
 
 
-def user_frequencies():
+def user_frequencies(cli_string):
     """
-    Takes user frequencies and returns them in a list when user specifies is complete
+    Takes user frequencies as space separated string and returns them as a list of integers
+    :param cli_string: str, space separated integers
     :return: list of user inputted integers
     """
-    user_input = []
-    while True:
-        entry = input('Please enter wavenumber (cm^-1) or \'run\'  : ')
-        if entry == 'run':
-            break
-        elif entry.isnumeric():
-            user_input.append(int(entry))
-
+    user_input = [int(i) for i in cli_string.split(' ')]
     return user_input
 
 
@@ -34,7 +29,7 @@ def ir_database_parser(user_input_list, ir_data):
     for freq in user_input_list:
         matches = [count for count, row in ir_data.iterrows() if row['min_abs(cm-1)'] <= freq <= row['max_abs(cm-1)']]
         freq_matches = ir_data.iloc[matches]
-        freq_matches.insert(loc=0, column='user', value=freq)  # insert user frequency for reference in the saved file
+        freq_matches.insert(loc=0, column='User', value=freq)  # insert user frequency for reference in the saved file
         user_df = user_df.append(freq_matches)
 
     return user_df
@@ -42,16 +37,22 @@ def ir_database_parser(user_input_list, ir_data):
 
 ########################################################################################################################
 
-
-def main():
+@click.command()
+@click.option('--ref_file', '-r')
+@click.option('--wavenumbers', '-w')
+@click.option('--save_name', '-s')
+def main(ref_file, wavenumbers, save_name):
     """
     Loads contents of an IR reference file to pandas datframe which then indices for matches to user specified
-    frequencies. The resulting dataframe is then saved to a csv file with user specified name.
+    wavenumbers. The resulting dataframe is then saved to a csv file with user specified name.
+    :param ref_file: location of reference IR file, pipe delimited as commas present in chemical nomenclature
+    :param wavenumbers: str, space separated user inputs corresponding to peaks in their IR spectrum
+    :param save_name: str, the filename (including extension) of the results to be saved to
+    :return: none
     """
-    df = pd.read_csv('ir_frequencies.txt', sep='|')
-    frequencies = user_frequencies()
+    df = pd.read_csv(ref_file, sep='|')
+    frequencies = user_frequencies(wavenumbers)
     parsed_df = ir_database_parser(frequencies, df)
-    save_name = input('Please enter a filename to save to plus extension: ')
     parsed_df.to_csv(save_name, index=False)
 
 
