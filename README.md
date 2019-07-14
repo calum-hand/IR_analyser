ce2c63c124dd6900a65bc84d555773f2adb9f539
BundabergJoey <calumhand94@hotmail.com>
2019-07-14 21:29:09 +0100

    updated to use pandas now that I'd gotten everything else to work with standard library as per the original point

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # IR_analyser
 
 ## Project Function
-> Using Python, the aim is to allow for quicker analysis of standard, chemical Infrared (IR) Spectra. 
+> Using Python, the aim is to allow for quicker analysis of standard, chemical Infrared (IR) Spectra.
 
 ### Current Status
 > The program currently works as described below:
@@ -9,23 +9,21 @@
 >>* The script then takes the usre input (stored as a list) and then loops through the [database file](https://www.sigmaaldrich.com/technical-documents/articles/biology/ir-spectrum-table.html)
 >>* If the user input matches a value in the range of the peaks, then that user input and peak information is written to new list
 >>* Once the wavenumbers have been passed through the database, the loop stops
->>* The user is then prompted to enter a filename for the sorted peaks to be written to and saved 
+>>* The user is then prompted to enter a filename for the sorted peaks to be written to and saved
 
 
 ### TODO based on [r/learnpython](https://www.reddit.com/r/learnpython/comments/a5yriu/anyone_willing_to_help_review_an_beginners_code/) feedback:
->* Read database IR file with CSV module based on pipe delimiter
->* convert wavenumbers to integers while creating the database rather than convert later on during analysis
+>* ~~Read database IR file with CSV module based on pipe delimiter~~
+>* ~~convert wavenumbers to integers while creating the database rather than convert later on during analysis~~
 >* add kwargs in the main to allow custom file name inputs (when run as python simple_analysis.py some_file.txt) or some other way of not hardcoding the input name.
->* Instead of if 2 in range(1,5), do 1<=x<5, that way you do not need to expand the range for each entry.
->* unpack things like these to variables with informative names, eg : 
->> `for entry in data:`
-        `abs_min, abs_max = entry[:2]`
-        `if user_freq in range(abs_min, abs_max)`
+>* ~~Instead of if 2 in range(1,5), do 1<=x<5, that way you do not need to expand the range for each entry.~~
+>* ~~unpack things like these to variables with informative names, eg~~ :
+
+        for entry in data:
+                abs_min, abs_max = entry[:2]
+                if user_freq in range(abs_min, abs_max)
 
 
 ### Future works
 > While this project currently functions at a basic level the below updates are required or would like to be incorporated:
 >>* To be able to take an image of an IR spectra as input rather than user inputting the numbers
-
-
- 
--- interpretation/simple_analysis.py
+++ interpretation/simple_analysis.py
@@ -1,11 +1,4 @@
-########################################################################################################################
-
-
-def ir_database(path_to_file='ir_frequencies.txt'):
-    """Reads text file and then returns as nested array including headers"""
-    with open(path_to_file, 'r') as f:
-        return [line.split('|') for line in f.readlines()]
-
+import pandas as pd
 
 ########################################################################################################################
 
@@ -27,46 +20,26 @@ def user_frequencies():
 
 
 def ir_database_parser(user_input_list, ir_data):
-    """Takes user input as input then parses loaded IR database for relevant rows, will then return relevant row"""
-    parsed_frequencies = [['User_input']+ir_data[0]]  # add headers to the array that will be returned
-    for user_freq in user_input_list:  # for every frequency inputted by the user
-        for entry in ir_data[1:]:  # for every row in the loaded IR database (excluding the headers)
-            if user_freq in range(int(entry[0]), int(entry[1])):  # builds search range from min and max wavenumbers
-                parsed_frequencies.append([user_freq]+entry)  # adds user input wavenumber to start or appended list
+    user_df = pd.DataFrame()
+    for freq in user_input_list:
+        matches = [count for count, row in ir_data.iterrows() if row['min_abs(cm-1)'] <= freq <= row['max_abs(cm-1)']]
+        freq_matches = ir_data.iloc[matches]
+        freq_matches.insert(loc=0, column='user', value=freq)
+        user_df = user_df.append(freq_matches)
 
-    return parsed_frequencies  # returns nested array which is a trimmed down version of the original database
+    return user_df
 
-
-########################################################################################################################
-
-
-def results_writer(filtered_ir):
-    """Takes nested list as input and writes the result to a txt file with name specified by the user"""
-    while True:
-        user_file_name = input('Please enter a file name to save results to: ')
-        if user_file_name.isalpha():
-            print('Filename saved to :  '+user_file_name+'.txt')
-            break
-        else:
-            print('Please enter valid filename: ')
-
-    with open(user_file_name+'.txt', 'w') as f:  # writes nested array as a pipe delimited file
-        for line in filtered_ir:
-            for entry in line:
-                f.write(str(entry)+'|')
-
-    print('file written')
 
 ########################################################################################################################
 
 
 def main():
-    """Takes database file and user input to parse the database file, return a trimmed file and then save to new txt"""
-    returned_analysis = ir_database_parser(user_frequencies(), ir_database())  # create filtered list of wavenumbers
-    results_writer(returned_analysis)  # write the filtered list of wavenumbers to a file
+    df = pd.read_csv('ir_frequencies.txt', sep='|')
+    frequencies = user_frequencies()
+    parsed_df = ir_database_parser(frequencies, df)
+    save_name = input('Please enter a filename to save to plus extension: ')
+    parsed_df.to_csv(save_name, index=False)
 
-
-########################################################################################################################
 
 if __name__ == '__main__':
     main()
