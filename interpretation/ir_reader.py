bf168afb6a15b542bb4d5db18d38b430e853bcfe
Calum Hand <crh53@bath.ac.uk>
2019-07-18 01:03:17 +0100

    the bloomin file keeps rotating !!!

--- interpretation/ir_reader.py
+++ interpretation/ir_reader.py
@@ -1,10 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from scipy.signal import find_peaks, argrelmax
 
-im = cv2.imread('rough_ir.PNG')  # read and convert to numpy array
-non_white = np.where(im != 255)  # find coordinates of non white cells
-y, x, z = non_white  # unpack values (intentionally unpack as y, x, z due to how arrays are returned)
+plt.rcParams['figure.figsize'] = (15,5)
 
-plt.plot(x[::10], y[::10], '.',  color='green')  # proof of extraction
+im = cv2.imread('rough_ir.PNG')  # read image and convert to numpy array
+non_white = np.where(np.rot90(im,4) != 255)  # find coordinates of non white cells to extract plot
+a, b, c = non_white  # unpack arrays
+
+
+
+plt.plot(b, a, '.')
+plt.scatter(1155,22, color='red', label='Array start (bottom right)')
+plt.legend()
+plt.savefig('created_images/plotted_as_b_a.PNG', bbox_inches='tight')
+plt.show()
+
+plt.plot(a, '.')
+plt.legend()
+plt.savefig('created_images/plot_of_a.PNG', bbox_inches='tight')
+plt.show()
+
+plt.plot(b, '.')
+plt.legend()
+plt.savefig('created_images/plot_of_b.PNG', bbox_inches='tight')
 plt.show()
