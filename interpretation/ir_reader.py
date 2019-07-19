6e91f6b2bb2f8d7739f861a5a752400e42b6a807
Calum Hand <crh53@bath.ac.uk>
2019-07-19 14:18:22 +0100

    updated knowledge of why the read out isn't perfect

--- interpretation/ir_reader.py
+++ interpretation/ir_reader.py
@@ -1,28 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from scipy.signal import find_peaks, argrelmax
+from scipy.signal import find_peaks
 
 plt.rcParams['figure.figsize'] = (15,5)
 
 im = cv2.imread('rough_ir.PNG')  # read image and convert to numpy array
-non_white = np.where(np.rot90(im,4) != 255)  # find coordinates of non white cells to extract plot
+non_white = np.where(im != 255)  # find coordinates of non white cells to extract plot
 a, b, c = non_white  # unpack arrays
 
 
-
-plt.plot(b, a, '.')
-plt.scatter(1155,22, color='red', label='Array start (bottom right)')
-plt.legend()
-plt.savefig('created_images/plotted_as_b_a.PNG', bbox_inches='tight')
-plt.show()
-
-plt.plot(a, '.')
-plt.legend()
-plt.savefig('created_images/plot_of_a.PNG', bbox_inches='tight')
-plt.show()
-
-plt.plot(b, '.')
-plt.legend()
-plt.savefig('created_images/plot_of_b.PNG', bbox_inches='tight')
+plt.subplot(131)
+plt.plot(b[:50000], '.', color='blue')
+plt.title('first half')
+plt.subplot(132)
+plt.plot(b[50000:], 'x', color='red')
+plt.title('second half')
+plt.subplot(133)
+plt.plot(b, 'x', color='green')
+plt.title('full')
+plt.savefig('created_images/proof_of_irregular_array_creation.PNG', bbox_inches='tight')
 plt.show()
