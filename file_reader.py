5d182f15de2f4f4212882eff3ab2773c304cb1a3
BundabergJoey <calumhand94@hotmail.com>
2019-08-13 23:22:07 +0100

    functionalised the analysis of the IR image and ready to move into main code base

--- file_reader.py
+++ file_reader.py
@@ -1,15 +1,59 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import find_peaks
 
-im = cv2.imread('test_ir.PNG')  # read image as numpy array
-y, x, z = np.where(im < 245)  # get indices where the value is non white / white noise
-sorted = np.argsort(x)  # sort the index values to read left to right top to bottom
-x, y = x[sorted], y[sorted]  # re-sort
+########################################################################################################################
 
-plt.plot(x[:5000], y[:5000], '.', label='first')  # PROOF
-plt.plot(x[5000:10000], y[5000:10000], '.', label='second')
-plt.plot(x[10000:15000], y[10000:15000], '.', label='third')
-plt.plot(x[15000:], y[15000:], '.', label='fourth')
-plt.legend()
-plt.show()
+
+def spectrum_image_data(path_to_image):
+    """
+    Translates the passed IR spectrum image into an ordered numpy array. Only non white cells are considerd however the
+    np.where filtering also removes some white noise by filtering based on < 245 rather than just != 255
+
+    :param path_to_image: str, file path to the ir spectrum image to be analysed
+
+    :return: numpy.ndarray, the sequential intensity values of the retrieved IR data
+    """
+    im = cv2.imread(path_to_image)  # read image as numpy array
+    rgb_filter_value = 245
+    y, x, z = np.where(im < rgb_filter_value)  # get indices where the value is non white / white noise
+    sorted = np.argsort(x)  # sort the index values to read left to right top to bottom
+    y = y[sorted]  # re-sort
+    return y
+
+
+########################################################################################################################
+
+def ir_peak_locator(data, range_min, range_max):
+    """
+    Normalises the IR data extracted from the image previously to the wavenumber range of the IR spectrum itself.
+    This assumes a linear scale across the entirety of the original IR spectrum, finds relative percentages of the peak
+    locations and translates that to the original wavenumber range.
+
+    To ensure a suitable number of peaks are found and to prevent noisy peaks from being flagged, the distance between
+    peaks is iteratively examined and only if fewer than 30 peaks are found does the function proceed.
+
+    :param data: np.ndarray, the sequential intensity values of the retrieved IR data
+    :param range_min: int, the minimum value of the IR spectrum range in wavenumbers (typically 500 - 850)
+    :param range_max:  int, the maximum value of the IR spectrum range in wavenumbers (typically 3500 - 4000)
+
+    :return: numpy.ndarray, the rebased peak locations in wavenumbers
+    """
+    minimum_permitted_peaks = 30
+
+    for i in range(1,len(data), 10):  # as an element per pixel this is a huge range, step count 10 to reduce time
+        peak_locations = find_peaks(data, distance=i)[0]  # not interested in peak properties so only need locations
+
+        if len(peak_locations) <= minimum_permitted_peaks:
+            relative_peaks_percent = peak_locations[::-1] / len(data)  # image is upside down so need to invert range
+            wavenumbers_range = range_max - range_min
+            rebased_peak_locations = range_min + (relative_peaks_percent * wavenumbers_range)  # calculate wavenumbers
+            return rebased_peak_locations
+
+
+########################################################################################################################
+
+ir_data = spectrum_image_data('test_ir.PNG')
+peaks = ir_peak_locator(ir_data, 500, 3500)
+print(peaks)
+
